5e536ef52b5ed08d8ff94b16504edcaf213e7e9c
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-07-02 23:19:48 -0700

    for loop/ first test if-else statement

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function robogersSound(userNumber) {
+    let outputArray = [];
+    for (let i = 0; i <= userNumber; i++) {
+        if (i === 1) {
+            outputArray.push("Beep!");
+        }
+
+    }
+}
