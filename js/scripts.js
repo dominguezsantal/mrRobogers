ef28eccb3102952397adb298871205a8230273ce
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-07-02 23:30:14 -0700

    if/else statements defining second, and third TDD

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
+//Business Logic
+
 function robogersSound(userNumber) {
     let outputArray = [];
     for (let i = 0; i <= userNumber; i++) {
         if (i === 1) {
             outputArray.push("Beep!");
+        } else if (i === 2) {
+            outputArray.push("Boop!");
+        } else if (i === 3) {
+            outputArray.push("Won't you be my neighbor ?");
+        } else if (i.toString().includes(3)) {
+            outputArray.push("Won't you be my neighbor?");
+        } else if (i.toString().includes(2)) {
+            outputArray.push("Boop!");
+        } else if (i.toString().includes(1)) {
+            outputArray.push("Beep!");
+        } else {
+            outputArray.push(i);
         }
-
     }
+    return outputArray
+
 }
+
+
+//UI
